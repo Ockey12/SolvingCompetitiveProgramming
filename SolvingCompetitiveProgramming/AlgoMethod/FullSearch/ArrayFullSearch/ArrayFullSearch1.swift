8d41d1f69e924f0016b65cb48e7e26992b0e3d69
OnagaHaruki <haloupgarou@gmail.com>
2023-05-29 13:27:55 +0900

    add: arrayFullSearch1
    正解できた

---
+++ SolvingCompetitiveProgramming/AlgoMethod/FullSearch/ArrayFullSearch/ArrayFullSearch1.swift
@@ -0,0 +1,39 @@
+//
+//  ArrayFullSearch1.swift
+//  SolvingCompetitiveProgramming
+//
+//  Created by オナガ・ハルキ on 2023/05/29.
+//
+
+import Foundation
+
+func arrayFullSearch1() {
+    let (_, V) = readTwoInts()
+    let array = readIntsArray()
+    
+    for i in array {
+        if i == V {
+            print("Yes")
+            return
+        }
+    }
+    print("No")
+    
+    // MARK: Functions
+    
+    /// 標準入力で" "で区切られた2つの整数を表す文字列1行を受け取る．
+    /// 文字列をそれぞれInt型にキャストして2つのInt型の値を持つタプルを返す．
+    /// - Returns: 標準入力で受け取った(Int, Int)型のタプル1つ
+    func readTwoInts() -> (a: Int, b: Int) {
+        let ints = readLine()!.split(separator: " ").map { Int(String($0))! }
+        return (a: ints[0], b: ints[1])
+    }
+    
+    /// 標準入力で" "で区切られた複数の整数を表す文字列1行を受け取る．
+    /// 文字列をそれぞれInt型にキャストしてInt型の配列を返す．
+    /// - Returns: 標準入力で受け取った[Int]型の配列1つ．
+    func readIntsArray() -> [Int] {
+        return readLine()!.split(separator: " ").map { Int(String($0))! }
+    }
+}
+
